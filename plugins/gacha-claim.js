fc94dad14cbab52fe630bccd3ad95f8aab8b01ec
Neykoor💜 <chinquepapa@gmail.com>
2025-11-26 13:12:08 -0800

    Update gacha-claim.js

--- plugins/gacha-claim.js
+++ plugins/gacha-claim.js
@@ -16,7 +16,7 @@ const verifi = async () => {
   try {
     const pkg = await fs.readFile("./package.json", 'utf-8');
     const json = JSON.parse(pkg);
-    return json.repository?.["url"] === "git+https://github.com/Aqua200/Ouka-MD.git";
+    return json.repository?.["url"] === "git+https://github.com/ScriptNex/Legacy-MD.git";
   } catch {
     return false;
   }
@@ -24,7 +24,7 @@ const verifi = async () => {
 
 let handler = async (m, { conn, usedPrefix, command }) => {
   if (!(await verifi())) {
-    return conn.reply(m.chat, `❀ El comando *<${command}>* solo está disponible para Ouka-MD.\n> https://github.com/Aqua200/Ouka-MD`, m);
+    return conn.reply(m.chat, `❀ El comando *<${command}>* solo está disponible para Legacy-MD.\n> https://github.com/ScriptNex/Legacy-MD.git`, m);
   }
 
   const chatData = global.db.data.chats?.[m.chat] || {};
