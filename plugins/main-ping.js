c6246e06a20a82557594edc98a482de94c965e9e
Neykoor💜 <chinquepapa@gmail.com>
2025-11-26 13:35:52 -0800

    Update main-ping.js

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -1,39 +1,18 @@
 import speed from 'performance-now'
-import os from 'os'
+import { spawn, exec, execSync } from 'child_process'
 
 let handler = async (m, { conn }) => {
-  let timestamp = speed()
-  let sentMsg = await conn.reply(m.chat, '❀ Calculando ping...', m)
-  let latency = speed() - timestamp
+let timestamp = speed()
+let sentMsg = await conn.reply(m.chat, '❀ Calculando ping...', m)
+let latency = speed() - timestamp
+exec(`neofetch --stdout`, (error, stdout, stderr) => {
+let child = stdout.toString("utf-8");
+let ssd = child.replace(/Memory:/, "Ram:")
 
-  const cpus = os.cpus()
-  const cpu = cpus[0].model
-  const cpuSpeed = cpus[0].speed > 0 ? cpus[0].speed : 'Desconocido'
-
-  const totalMem = os.totalmem() / (1024 ** 3)
-  const freeMem = os.freemem() / (1024 ** 3)
-  const usedMem = totalMem - freeMem
-
-  function formatUptime(seconds) {
-    const h = Math.floor(seconds / 3600)
-    const m = Math.floor((seconds % 3600) / 60)
-    const s = Math.floor(seconds % 60)
-    return `${h} horas, ${m} minutos, ${s} segundos`
-  }
-
-  const uptime = formatUptime(os.uptime())
-
-  let result = `
-*» Velocidad* : ${latency.toFixed(0)} *_ms_*
-*» Procesador* : ${cpu}
-*» CPU* : ${cpuSpeed} MHz
-*» RAM* : ${usedMem.toFixed(2)} GB / ${totalMem.toFixed(2)} GB
-*» Tiempo activo* : ${uptime}
-  `
-
-  conn.sendMessage(m.chat, { text: result, edit: sentMsg.key }, { quoted: m })
+let result = `✰ *¡Pong!*\n> Tiempo ⴵ ${latency.toFixed(4).split(".")[0]}ms\n${ssd}`
+conn.sendMessage(m.chat, { text: result, edit: sentMsg.key }, { quoted: m })
+})
 }
-
 handler.help = ['ping']
 handler.tags = ['info']
 handler.command = ['ping', 'p']
